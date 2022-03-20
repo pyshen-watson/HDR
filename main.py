7251ecf0c574de04f506e54d4b8986462cd0544c
pyshen <pyshen.watson@gmail.com>
2022-03-21 04:15:47 +0800

    Simplifed the interface

--- main.py
+++ main.py
@@ -1,12 +1,21 @@
-from modules.downloadSourceImages import download
+import os
+from modules.utils import selection, download, album_names
+from modules.VFXimage import VFXImageSet
 
 
 if __name__ == '__main__':
 
-    try:
-        album_id = input('Enter the album id: [0]Test [1]NTULibrary (default none): ')
-        album_id = int(album_id)
+    album_id = selection(
+                statement='Select the album: [0]Test [1]NTULibrary (default 0): ',
+                N_options=2,
+                default_value=0)
+
+
+    album_path = f'./Images/{album_names[album_id]}/original'
+    if not os.path.isdir(album_path):
         download(album_id)
-    except:
-        pass
+
+
+    images = VFXImageSet(album_path)
+    images.align()
 
